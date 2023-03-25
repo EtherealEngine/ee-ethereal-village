41488addba8e58b94c6a0546eec8f57df2c9a556
HexaField <joshfield999@gmail.com>
2023-03-25 18:32:30 +1100

    Initial Commit

---
+++ xrengine.config.ts
@@ -0,0 +1,11 @@
+import type { ProjectConfigInterface } from '@etherealengine/projects/ProjectConfigInterface'
+
+const config: ProjectConfigInterface = {
+  onEvent: undefined,
+  thumbnail: '/static/etherealengine.png',
+  routes: {},
+  services: undefined,
+  databaseSeed: undefined
+}
+
+export default config
