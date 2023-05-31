6d427bb699d0397afacccbac81ed1bd63a005dce
HexaField <joshfield999@gmail.com>
2023-05-31 13:29:53 +1000

    update thumbnail in config

--- xrengine.config.ts
+++ xrengine.config.ts
@@ -1,8 +1,10 @@
 import type { ProjectConfigInterface } from '@etherealengine/projects/ProjectConfigInterface'
 
+import commonConfig from '@etherealengine/common/src/config'
+
 const config: ProjectConfigInterface = {
   onEvent: undefined,
-  thumbnail: '/static/etherealengine.png',
+  thumbnail: commonConfig.client.fileServer + '/projects/ee-ethereal-village/thumbnail.png',
   routes: {},
   services: undefined,
   databaseSeed: undefined
